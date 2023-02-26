aaf52754e795ae066e2aa8850cbf1cba482bb354
heioel <tukilmuah@gmail.com>
2023-02-26 10:20:05 +0700

    menambah file aritmatika dan array, serta merubah isi index.html

---
+++ array.js
@@ -0,0 +1,17 @@
+let myArr = ['helo','my','friend','im','so','happy']
+
+console.log(myArr);
+
+// let searchArr = myArr.includes('my');
+// console.log(searchArr);
+// for(let i = 0; i < myArr.length; i++){
+//     console.log(myArr[i]);
+// }
+
+let arrInArr = [
+    [1,2,3,4,5],
+    ['satu','dua','tiga','empat','lima'],
+    [true,false]
+]
+
+console.log(arrInArr[0][2]);
